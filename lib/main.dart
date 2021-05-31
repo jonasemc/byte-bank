303d987c011486e27b5c96356d316e4efd720aac
jonas <jonasmirandaa21@gmail.com>
2021-05-30 22:55:47 -0300

    implementação de widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,32 @@
-void main() => runApp();
+import 'package:flutter/material.dart';
+
+void main() => runApp(Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              'Craft beautiful UIs',
+              textDirection: TextDirection.ltr,
+            ),
+          ],
+        ),
+        Expanded(
+          child: FittedBox(
+            fit: BoxFit.contain, // otherwise the logo will be tiny
+            child: FlutterLogo(),
+          ),
+        ),
+      ],
+    ));
